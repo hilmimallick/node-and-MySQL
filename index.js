260f660551cbddc9574e3b5dc397e8224b490f19
hilmimallick <hilmi09mallick@gmail.com>
2022-08-04 10:23:33 +0200

    testing

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require("express"); // Used to set up a server
 const cors = require("cors"); // Used to prevent errors when working locally
 const bodyParser = require("body-parser");
 const path = require("path");
-const _dirname = path.resolve();
+const _dirname = path.resolve("public");
 
 const app = express(); // Initialize express as an app variable
 app.set("port", process.env.PORT || 6969); // Set the port
 app.use(express.json()); // Enable the server to handle JSON requests
 app.use(cors()); // Dont let local development give errors
 app.use(express.static("public"));
-app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
