f874b6ffa00fd6a1f652b2924cfab1ff63b21828
hilmimallick <hilmi09mallick@gmail.com>
2022-08-04 10:45:58 +0200

    testing

--- index.js
+++ index.js
@@ -1,58 +1,89 @@
+// const express = require("express"); // Used to set up a server
+// const cors = require("cors"); // Used to prevent errors when working locally
+// const bodyParser = require("body-parser");
+// const path = require("path");
+// const _dirname = path.resolve("../public");
+
+// const app = express(); // Initialize express as an app variable
+// app.set("port", process.env.PORT || 6969); // Set the port
+// app.use(express.json()); // Enable the server to handle JSON requests
+// app.use(cors()); // Dont let local development give errors
+// app.use(express.static("public"));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// //for user
+// const userRoute = require("./routes/userRouter");
+// app.use("/users", userRoute);
+
+// //for product
+// const productRoute = require("./routes/productRouter");
+// app.use("/products", productRoute);
+
+// //for categories
+// const categoriesRoute = require("./routes/categoriesRouter");
+// app.use("/categories", categoriesRoute);
+
+// //for orders
+// const ordersRoute = require("./routes/ordersRouter");
+// app.use("/orders", ordersRoute);
+
+// //for order_details
+// const order_detailsRoute = require("./routes/order_detailsRouter");
+// app.use("/order_details", order_detailsRoute);
+
+// //for product_categories
+// const product_categoriesRoute = require("./routes/product_categoriesRouter");
+// app.use("/product_categories", product_categoriesRoute);
+
+// app.get("/", (req, res) => {
+//   res.sendFile(_dirname + "/" + "index.html");
+// });
+
+// app.listen(app.get("port"), () => {
+//   console.log(`http://localhost:${app.get("port")}`);
+//   console.log("Press Ctrl+C to exit server");
+// });
+
+// // app.get("/", function (req, res) {
+// //   res.sendFile(_dirname + "/" + "index.html");
+// // });
+
+// // app.get("/", function (req, res) {
+// //   res.sendFile(_dirname + "/" + "login.html");
+// // });
+
+// // app.get("/", function (req, res) {
+// //   res.sendFile(_dirname + "/" + "Products.html");
+// // });
+
 const express = require("express"); // Used to set up a server
 const cors = require("cors"); // Used to prevent errors when working locally
-const bodyParser = require("body-parser");
-const path = require("path");
-const _dirname = path.resolve("../public");
-
 const app = express(); // Initialize express as an app variable
 app.set("port", process.env.PORT || 6969); // Set the port
 app.use(express.json()); // Enable the server to handle JSON requests
 app.use(cors()); // Dont let local development give errors
 app.use(express.static("public"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//for user
-const userRoute = require("./routes/userRouter");
-app.use("/users", userRoute);
-
-//for product
-const productRoute = require("./routes/productRouter");
-app.use("/products", productRoute);
-
-//for categories
-const categoriesRoute = require("./routes/categoriesRouter");
-app.use("/categories", categoriesRoute);
-
-//for orders
-const ordersRoute = require("./routes/ordersRouter");
-app.use("/orders", ordersRoute);
-
-//for order_details
-const order_detailsRoute = require("./routes/order_detailsRouter");
-app.use("/order_details", order_detailsRoute);
-
-//for product_categories
-const product_categoriesRoute = require("./routes/product_categoriesRouter");
-app.use("/product_categories", product_categoriesRoute);
-
-app.get("/", (req, res) => {
-  res.sendFile(_dirname + "/" + "index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/" + "index.html");
 });
-
+const userRoute = require("./routes/userRouter");
+const productsRouter = require("./routes/productsRouter");
+const ordersRouter = require("./routes/ordersRoute");
+const categoriesRoute = require("./routes/categoriesRoute");
+const order_detailsRoute = require("./routes/order_detailsRoute");
+const product_categoriesRoute = require("./routes/product_categoriesRoute");
+app.use("/users", userRoute);
+app.use("/products", productsRouter);
+app.use("/orders", ordersRouter);
+app.use("/categories", categoriesRoute);
+app.use("/order_details", order_detailsRoute);
+app.use("/product_categories", product_categoriesRoute);
 app.listen(app.get("port"), () => {
-  console.log(`http://localhost:${app.get("port")}`);
+  console.log(`Listening for calls on port ${app.get("port")}`);
   console.log("Press Ctrl+C to exit server");
 });
-
-// app.get("/", function (req, res) {
-//   res.sendFile(_dirname + "/" + "index.html");
-// });
-
-// app.get("/", function (req, res) {
-//   res.sendFile(_dirname + "/" + "login.html");
-// });
-
-// app.get("/", function (req, res) {
-//   res.sendFile(_dirname + "/" + "Products.html");
-// });
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+  res.sendFile(_dirname + "/" + "login.html");
+});
